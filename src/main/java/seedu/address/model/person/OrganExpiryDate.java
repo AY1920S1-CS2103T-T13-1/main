26bbd37a58a5fceb941aa30b24e1d3bcd7e3b4fb
WilliamRyank <kwilliamryan@yahoo.com>
2019-10-12 13:41:30 +0700

    Add more testcases

--- src/main/java/seedu/address/model/person/OrganExpiryDate.java
+++ src/main/java/seedu/address/model/person/OrganExpiryDate.java
@@ -6,17 +6,14 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Represents a Donor organ's expiry date in ORGANice.
  * Guarantees: immutable; is valid as declared in {@link #isValidExpiryDate(String)} (String)}
  */
 public class OrganExpiryDate {
+
+    public static final DateFormat DATE_FORMAT = new SimpleDateFormat("dd-MMM-yyyy");
 
     public static final String MESSAGE_CONSTRAINTS = "Organ's expiry date must be in the format DD-MMM-YYYY"
             + " and is after current date. An example will be 27-Jan-2020";
@@ -31,11 +28,10 @@ public class OrganExpiryDate {
     public OrganExpiryDate(String expiryDate) {
         requireNonNull(expiryDate);
         try {
-            expiryDate = expiryDate.substring(0, 3) + expiryDate.substring(3, 4).toUpperCase()
-                    + expiryDate.substring(4).toLowerCase();
+            DateFormat dateFormat = new SimpleDateFormat("dd MMM yyyy");
             checkArgument(isValidExpiryDate(expiryDate), MESSAGE_CONSTRAINTS);
-            value = expiryDate;
-        } catch (StringIndexOutOfBoundsException e) {
+            value = dateFormat.format(DATE_FORMAT.parse(expiryDate)); //make the data value uniform
+        } catch (ParseException e) {
             throw new IllegalArgumentException();
         }
     }
@@ -44,10 +40,9 @@ public class OrganExpiryDate {
      * Returns true if a given string is a valid organ's expiry date.
      */
     public static boolean isValidExpiryDate(String test) {
-        DateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy");
-        formatter.setLenient(false);
         try {
-            formatter.parse(test);
+            DATE_FORMAT.setLenient(false);
+            DATE_FORMAT.parse(test);
             return true;
         } catch (ParseException e) {
             return false;
