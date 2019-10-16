b6658910f0652acd139b2b88401a9b2d36d1ab3b
Chow Jia Ying <chowjiaying211@gmail.com>
2019-10-16 14:54:56 +0800

    Add skeleton of MatchCommand

--- src/main/java/organice/logic/commands/MatchCommand.java
+++ src/main/java/organice/logic/commands/MatchCommand.java
@@ -2,7 +2,12 @@ package organice.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.function.Predicate;
+
 import organice.model.Model;
+import organice.model.person.Nric;
+import organice.model.person.Patient;
+import organice.model.person.Person;
 
 /**
  * Lists all persons in the address book to the user.
@@ -13,19 +18,36 @@ public class MatchCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Matches patients and donors who passes matching tests"
             + "Parameters: all/ (to match all patients and donors)"
-            + "Parameters: ic/NRIC (to match a patient of specified NRIC)"
+            + "Parameters: ic/NRIC (to match a patient of specified NRIC)";
+
     public static final String MESSAGE_SUCCESS = "Matched all patients and donors";
 
-    public MatchCommand(String input) {
+    Predicate<Person> matchesWithNric = donor -> donor.matches(patient);
 
+    private String input;
+    private Patient patient;
+
+    public MatchCommand(String input) {
+        this.input = input;
     }
 
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        runAllTests();
-        model.updateFilteredPersonList(results);
+
+        Nric patientNric = new Nric(input);
+
+        if (model.hasPatient(patientNric)) {
+            model.updateFilteredPersonList(matchesWithNric);
+        }
         return new CommandResult(MESSAGE_SUCCESS);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof MatchCommand // instanceof handles nulls
+                && input.equals(((MatchCommand) other).input)); // state check
     }
 }
