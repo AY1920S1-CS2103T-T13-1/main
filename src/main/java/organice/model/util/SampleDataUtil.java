5d787c7f553d46382c30a1af0301171a91cfb0fe
Chow Jia Ying <chowjiaying211@gmail.com>
2019-10-30 11:30:50 +0800

    Change dummy data to be populated

--- src/main/java/organice/model/util/SampleDataUtil.java
+++ src/main/java/organice/model/util/SampleDataUtil.java
@@ -2,25 +2,49 @@ package organice.model.util;
 
 import organice.model.AddressBook;
 import organice.model.ReadOnlyAddressBook;
+import organice.model.person.Age;
+import organice.model.person.BloodType;
+import organice.model.person.Doctor;
+import organice.model.person.DoctorInCharge;
+import organice.model.person.Donor;
 import organice.model.person.Name;
 import organice.model.person.Nric;
+import organice.model.person.Organ;
+import organice.model.person.OrganExpiryDate;
+import organice.model.person.Patient;
 import organice.model.person.Person;
 import organice.model.person.Phone;
+import organice.model.person.Priority;
+import organice.model.person.Status;
+import organice.model.person.TissueType;
 import organice.model.person.Type;
 
 /**
  * Contains utility methods for populating {@code AddressBook} with sample data.
  */
+
 public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         return new Person[] {
-            new Person(new Type("doctor"), new Nric("S1111111A"), new Name("Alex Yeoh"), new Phone("87438807")),
-            new Person(new Type("doctor"), new Nric("T1333333P"), new Name("Bernice Yu"), new Phone("99272758")),
-            new Person(new Type("doctor"), new Nric("G1234213L"), new Name("Charlotte Oliveiro"),
-                    new Phone("93210283")),
-            new Person(new Type("doctor"), new Nric("F5734625D"), new Name("David Li"), new Phone("91031282")),
-            new Person(new Type("doctor"), new Nric("S6243536R"), new Name("Irfan Ibrahim"), new Phone("92492021")),
-            new Person(new Type("doctor"), new Nric("T6746356G"), new Name("Roy Balakrishnan"), new Phone("92624417"))
+                new Doctor(new Type("doctor"), new Nric("S1111111A"), new Name("Alex Yeoh"), new Phone("87438807")),
+                new Donor(new Type("donor"), new Nric("S4444444R"), new Name("John Doe Donor 2"),
+                          new Phone("99999999"), new Age("17"), new BloodType("A"),
+                          new TissueType("1,2,3,4,5,6"), new Organ("kidney"),
+                          new OrganExpiryDate("23-Oct-2019"), new Status("not processing")),
+                new Donor(new Type("donor"), new Nric("S1212121A"), new Name("John Doe Donor"),
+                          new Phone("98799879"), new Age("30"), new BloodType("O"),
+                          new TissueType("1,2,3,4,7,8"), new Organ("kidney"),
+                          new OrganExpiryDate("24-Oct-2019"), new Status("not processing")),
+                new Patient(new Type("patient"), new Nric("S1234568R"), new Name("John Doe"), new Phone("98765432"),
+                            new Age("21"), new Priority("high"), new BloodType("A"), new TissueType("1,2,3,4,5,6"),
+                            new Organ("kidney"), new DoctorInCharge("S1111111A"), new Status("not processing")),
+                new Patient(new Type("patient"), new Nric("S4444444G"), new Name("Sam"), new Phone("999988888"),
+                            new Age("19"), new Priority("medium"), new BloodType("AB"), new TissueType("2,3,4,5,6,7"),
+                            new Organ("kidney"), new DoctorInCharge("S1111111A"), new Status("not processing")),
+                new Patient(new Type("patient"), new Nric("S9988776G"), new Name("Roy Kim"), new Phone("99944888"),
+                            new Age("33"), new Priority("low"), new BloodType("O"), new TissueType("6,7,8,9,10,11"),
+                            new Organ("kidney"), new DoctorInCharge("S1111111A"), new Status("not processing"))
+
         };
     }
 
@@ -31,6 +55,4 @@ public class SampleDataUtil {
         }
         return sampleAb;
     }
-
-
 }
