f7ca5c61a828ecca08c71e8c910eefb4e1f7d9a7
SimNee <chua_sim_nee@hotmail.com>
2019-10-31 03:18:37 +0800

    trying to revert the conflict removal

--- src/test/java/organice/logic/parser/DoneCommandParserTest.java
+++ src/test/java/organice/logic/parser/DoneCommandParserTest.java
@@ -1,11 +1,6 @@
 package organice.logic.parser;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static organice.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static organice.logic.parser.CommandParserTestUtil.assertParseSuccess;
-
 import org.junit.jupiter.api.Test;
-import organice.logic.commands.ListCommand;
 import organice.model.person.Type;
 
 class DoneCommandParserTest {
