5201a65530e11231d10ae88f0336a4530bc43162
Yen-Peng <e0318790@u.nus.edu>
2019-10-31 19:49:20 +0800

    Fix checkstyle.

--- src/main/java/organice/model/comparator/NameComparator.java
+++ src/main/java/organice/model/comparator/NameComparator.java
@@ -4,6 +4,10 @@ import java.util.Comparator;
 
 import organice.model.person.MatchedPatient;
 
+/**
+ * This comparator is used to sort a list of matched patients according to their name in alphabetical order.
+ * Order: A - Z.
+ */
 public class NameComparator implements Comparator<MatchedPatient> {
 
 
--- src/main/java/organice/model/comparator/NumOfMatchesComparator.java
+++ src/main/java/organice/model/comparator/NumOfMatchesComparator.java
@@ -4,6 +4,10 @@ import java.util.Comparator;
 
 import organice.model.person.MatchedPatient;
 
+/**
+ * This comparator is used to sort a list of matched patients according to the number of matched donors they have.
+ * Order: from patients with most matches to those with the least matches.
+ */
 public class NumOfMatchesComparator implements Comparator<MatchedPatient> {
 
     @Override
