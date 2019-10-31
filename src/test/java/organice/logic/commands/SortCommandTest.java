c50f4b415805f0de59ae16dbf62eaf1736a6ac18
Yen-Peng <e0318790@u.nus.edu>
2019-10-31 13:23:27 +0800

    Correct success rate comparison.

---
+++ src/test/java/organice/logic/commands/SortCommandTest.java
@@ -0,0 +1,17 @@
+package organice.logic.commands;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortCommandTest {
+
+
+    @Test
+    void execute() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+}
---
+++ src/test/java/organice/logic/parser/SortCommandParserTest.java
@@ -0,0 +1,14 @@
+package organice.logic.parser;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortCommandParserTest {
+
+    @Test
+    void parse_allFieldsPresent_success() {
+    }
+
+
+}
