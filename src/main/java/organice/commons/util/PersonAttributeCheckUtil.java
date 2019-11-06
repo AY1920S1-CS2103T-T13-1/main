1c283f05e42f2781a40d710eb47424a8de2e8af6
TheRealRavager <iskandar@u.nus.edu>
2019-11-06 11:46:11 +0800

    [Type Check Find] WIP add type check util class

---
+++ src/main/java/organice/commons/util/PersonAttributeCheckUtil.java
@@ -0,0 +1,20 @@
+package organice.commons.util;
+
+import organice.logic.parser.ArgumentMultimap;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Helper functions for checking properties or validity of Person and Person child class attributes.
+ */
+public class PersonAttributeCheckUtil {
+
+    checkValidtyAttributes(ArgumentMultimap argMultimap) {
+        // attempt to create the attribute from values in multimap
+    }
+
+    checkConditionAttributes(List<?> attributes, Predicate<?>) {
+        // return true if all attributes pass predicate
+    }
+}
