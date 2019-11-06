8e1bca17d737454130681bd1497e7f873dfd3766
SimNee <chua_sim_nee@hotmail.com>
2019-11-06 19:01:54 +0800

    a

--- src/main/java/organice/logic/parser/ListCommandParser.java
+++ src/main/java/organice/logic/parser/ListCommandParser.java
@@ -33,7 +33,7 @@ public class ListCommandParser implements Parser<ListCommand> {
             type = parseType(argMultimap);
             return new ListCommand(type);
         } else if (!args.trim().isEmpty()) {
-            throw new ParseException(
+            throw new Exception(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.MESSAGE_USAGE));
         }
         return new ListCommand(type); // if no type specified return list of all persons
